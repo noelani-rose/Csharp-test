e674824190f273c22049c6554dc1d112b4034f9d
Noelani West <noelani.rose14@gmail.com>
2023-02-23 12:00:31 -0600

    create Car class and instance of newCar

--- Program.cs
+++ Program.cs
@@ -5,13 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace SimpleClasses
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar  = new Car();
+            myCar.Make = "Mazda";
+            myCar.Model = "CX5";
+            myCar.Year = 2017;
+            myCar.Color = "Black";
+
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myCar.Make, 
+                myCar.Model, 
+                myCar.Year, 
+                myCar.Color);
+
+            Console.ReadLine();
         }
     }
+    class Car
+    {
+        public string Make {get; set;}
+        public string Model {get; set;}
+        public int Year {get; set;}
+        public string Color {get; set;}
+
+    }
+    
 }
