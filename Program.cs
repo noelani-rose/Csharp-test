3b58bd43b5a7df61f3a432a62a3f2cc247c60a33
Noelani West <noelani.rose14@gmail.com>
2023-02-23 10:52:28 -0600

    set up template console app

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HelloWorld
 {
